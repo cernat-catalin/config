b1dee94bfcbe09b8c1cff0cb5add70c7132f60b2
ccernat <catalinstefan.cernat@teampicnic.com>
2022-01-27 17:20:21 +0100

    NVIM - Git plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 --[[
---  Limits of lua API and how to interact with vimscript:
+--  Limit of lua API and how to interact with vimscript:
 --      1. `vim.cmd` is used for ... XXX
 --      2. `vim.fn` is used for ... XXX
 --      3. `vim.g` is used for ... XXX
@@ -18,6 +18,8 @@
 -- XXX: Configure Telescope
 -- XXX: Migrate vimwiki scripts from vimscript to lua
 -- XXX: Avoid LSP in comments!
+-- XXX: Standardize tab indent size
+-- XXX: LSP and Git keymaps
 
 require('packer').startup(function()
   -- Package Management
@@ -84,6 +86,18 @@ require('packer').startup(function()
   -- Clojure XXX
   use 'Olical/conjure'
   use 'Olical/aniseed'
+
+
+  -- Git support
+  use {
+  'lewis6991/gitsigns.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim'
+  },
+  config = function()
+    require('gitsigns').setup()
+  end
+}
 end)
 
 
@@ -91,6 +105,9 @@ end)
 require('_common_settings')
 require('_which-key')
 require('_lsp')
+
+-- Setup Git plugin
+--require('gitsigns').setup()
 
 
 -- Tree Sitter
@@ -105,3 +122,35 @@ else
     vim.api.nvim_set_keymap('', '<C-_>', '<plug>NERDCommenterToggle', {})
 end
 
+
+require('gitsigns').setup {
+  on_attach = function(bufnr)
+    local function map(mode, lhs, rhs, opts)
+        opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
+    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+
+    -- Actions
+    map('n', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
+    map('v', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
+    map('n', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
+    map('v', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
+    map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+    map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+    map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+    map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+    map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+    --map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+    map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+    map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+    --map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+
+    -- Text objec
+    map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
