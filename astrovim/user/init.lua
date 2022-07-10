9a5865fc9db488461798721af209dfa82b765d81
ccernat <catalinstefan.cernat@teampicnic.com>
2022-07-10 17:37:52 +0200

    ASTROVIM

--- astrovim/user/init.lua
+++ astrovim/user/init.lua
@@ -5,7 +5,18 @@ local config = {
       config.scroll_limit = 0
         return config
     end,
+  },
+  mappings = {
+    n = {
+      ["<leader>ct"] = { function() require("Comment.api").toggle_current_linewise() end, desc = "Comment line" }
+    },
+    v =  {
+    ["<leader>ct"] = {
+      "<esc><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<cr>",
+      desc = "Toggle comment line",
+    }
   }
+  },
 }
 
 return config
