e0ccb52877a733e45deb86f101b1285ae800392e
cernat-catalin <cernat.catallin@gmail.com>
2023-04-28 11:16:44 +0200

    Stylistic changes across the board
    - [alacritty] Change colorscheme and add window opacity
    - [alacritty] Remove commented out example configurations
    - [nvim] Add transparency plugin
    - [nvim] Fix which-key missing symbols
    - [tmux] Change status bar colors

--- nvim/user/init.lua
+++ nvim/user/init.lua
@@ -1,5 +1,32 @@
+-- Enable transparency by default
+vim.g.transparent_enabled = true
+
+
 local config = {
-  -- Configure plugins
+  plugins = {
+    {
+      "xiyaowong/transparent.nvim",
+      lazy = false,
+      config = function()
+        require("transparent").setup({
+          groups = { -- table: default groups
+            'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+            'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+            'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+            'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+          },
+          extra_groups = {
+            "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+            "NeoTreeNormal",
+            "NeoTreeNormalNC",
+            "NormalFloat",
+          },                   -- table: additional groups that should be cleared
+          exclude_groups = {}, -- table: groups you don't want to clear
+        })
+      end
+    },
+    { "folke/which-key.nvim", opts = { icons = { separator = "➜" } } }
+  },
   mappings = {
     n = {
       ["<leader>ct"] = { function() require("Comment.api").toggle.linewise() end, desc = "Comment line" },
